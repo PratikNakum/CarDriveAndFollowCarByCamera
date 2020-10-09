fb82d6bb24de35e2afb41eee256787af51763e7f
Pratik Nakum <51849828+PratikNakum@users.noreply.github.com>
2020-10-09 17:42:38 +0530

    Add files via upload

---
+++ Scripts/CarManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Tag.Pratik{
+public class CarManager : MonoBehaviour
+{
+        #region PUBLIC_VARS
+        public CarMovement[] car; 
+        #endregion
+
+        #region PRIVATE_VARS
+        private int count = 1;
+        private CarMovement curruntCar ;
+        #endregion
+
+        #region UNITY_CALLBACKS
+        private void Start()
+        {
+            curruntCar = car[count];
+        }
+        private void Update()
+        {
+            ChangeCar();
+            curruntCar.MoveCar();
+        }
+        private void LateUpdate()
+        {
+            curruntCar.FollowCar();
+            curruntCar.GetInBoundry();
+        }
+        #endregion
+
+        #region PUBLIC_FUNCTIONS
+        #endregion
+
+        #region PRIVATE_FUNCTIONS
+
+        private void ChangeCar()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                curruntCar = car[count % car.Length];
+                count++;
+            }
+        }
+
+        #endregion
+
+        #region CO-ROUTINES
+        #endregion
+
+        #region EVENT_HANDLERS
+        #endregion
+
+        #region UI_CALLBACKS
+        #endregion
+    }
+}
---
+++ Scripts/CarMovement.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Tag.Pratik{
+public class CarMovement : MonoBehaviour
+{
+        #region PUBLIC_VARS
+        public float carSpeed;
+        public Renderer groundPlan;
+        public Transform cameraObject;
+        public Vector3 cameraPositionBehindcar;
+        #endregion
+
+        #region PRIVATE_VARS
+        private float cameraMoveSpeed;
+        private float carRotationSpeed;
+        #endregion
+
+        #region UNITY_CALLBACKS
+        private void Start()
+        {
+            cameraMoveSpeed = 1;
+            carRotationSpeed = 1;
+             carSpeed = 30;
+        }
+        //private void Update()
+        //{
+        //    MoveCar();
+        //}
+      
+
+
+        #endregion
+
+        #region PUBLIC_FUNCTIONS
+        public void MoveCar()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += transform.forward * carSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += (-transform.forward) * carSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.down * carRotationSpeed );
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(Vector3.up * carRotationSpeed);
+            }
+        }
+        public void FollowCar()
+        {
+            cameraObject.LookAt(transform.position);
+            Vector3 cameraBehindCar = transform.TransformPoint(cameraPositionBehindcar);
+            cameraObject.position = Vector3.Lerp(cameraObject.position, cameraBehindCar, cameraMoveSpeed * Time.deltaTime);
+        }
+
+        public void GetInBoundry()
+        {
+            float maxRediousOfCircle = FindMaxRadiousOfCircleOnPlane();
+            transform.position = Vector3.ClampMagnitude(transform.position, maxRediousOfCircle);
+        }
+    
+        private float FindMaxRadiousOfCircleOnPlane()
+        {
+           return Mathf.Min(groundPlan.bounds.size.x,groundPlan.bounds.size.z)/2;   // groundPlan.bounds.size.z -->find the length of the plane
+            
+        }
+        #endregion
+
+        #region PRIVATE_FUNCTIONS
+        #endregion
+
+        #region CO-ROUTINES
+        #endregion
+
+        #region EVENT_HANDLERS
+        #endregion
+
+        #region UI_CALLBACKS
+        #endregion
+    }
+}
